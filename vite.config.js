5237156cafb22cdf9a24e65f5077678acde0878c
asharglobalweb <ashar@globalweb.ae>
2026-02-20 09:16:51 +0400

    vite vonfig error

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { createHtmlPlugin } from 'vite-plugin-html'
+import { createHtmlPlugin } from 'vite-plugin-html';
 import { siteData } from './src/data/data.js' // Import your data
+
 
 export default defineConfig({
   plugins: [
